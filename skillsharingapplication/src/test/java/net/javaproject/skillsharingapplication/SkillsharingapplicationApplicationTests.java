36823954a68ad1789c2be69ea536a221b46e7371
joanroche444 <joanroche1604@gmail.com>
2025-04-25 20:26:51 +0530

    Spring Boot project structure

---
+++ skillsharingapplication/src/test/java/net/javaproject/skillsharingapplication/SkillsharingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaproject.skillsharingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsharingapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
